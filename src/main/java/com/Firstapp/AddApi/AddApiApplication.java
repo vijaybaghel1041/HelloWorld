147ca875490e154fa167e5c7a333062eb545c36f
vijaybaghel1041-admin <vijaybaghel1041@gmail.com>
2025-01-04 19:12:55 +0530

    intial committt

---
+++ src/main/java/com/Firstapp/AddApi/AddApiApplication.java
@@ -0,0 +1,13 @@
+package com.Firstapp.AddApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AddApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AddApiApplication.class, args);
+	}
+
+}
